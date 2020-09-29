c72bcc1d44480014385d7330880d5a8fdddf70a5
Kamitotomi <rockyaaokage@gmail.com>
2020-09-29 23:43:50 +0900

    on

---
+++ Chapter02/all/js/main.js
@@ -0,0 +1,16 @@
+// $(function(){
+	// $('#typo .inner').css('transform', 'rotate(10deg');
+// });
+
+// $(function(){
+	// $('.page-main > div:nth-child(1) .inner').css('opacity', 0.5);
+// });
+
+$(function () {
+	// body...
+	$('#typo').on('mouseover', function(){
+		$('#typo').css('color', '#ebc000');
+	});
+});
+
+// 引数一つ目がイベントタイプ、二つ目の関数がハンドラ
