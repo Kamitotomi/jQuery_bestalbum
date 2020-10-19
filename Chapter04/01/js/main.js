6f939a5f50d884e8a8b89afadd955d31f4549305
Kamitotomi <rockyaaokage@gmail.com>
2020-10-19 23:11:40 +0900

    Eagle

--- Chapter04/01/js/main.js
+++ Chapter04/01/js/main.js
@@ -18,5 +18,20 @@ $(function () {
                 backgroundColor: 'fff',
                 color: '#ebc000'
             }, duration);
-        })
-})
+        });
+
+        $('#buttons1 button:nth-child(n+5):nth-child(-n+8)')
+            .on('mouseover', function(){
+                $(this).stop(true).animate({
+                    borderWidth: '12px',
+                    color: '#ae5e9b'
+                }, duration, 'easeOutSine');
+            })
+            .on('mouseout', function(){
+                // body...
+                $(this).stop(true).animate({
+                    borderWidth: '0px',
+                    color: '#ebc000'
+                }, duration, 'easeOutSine');
+            });
+});
